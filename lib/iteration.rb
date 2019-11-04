765c71f33badfe8d75d284137ff8af4c95caae77
Ayako Wiltshire <awittyl44@gmail.com>
2019-11-04 15:49:33 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -44,7 +44,7 @@ def total_even_pairs(src)
   while row_index < src.count do 
     element_index = 0
   while element_index < src[row_index].count do
-    if src[row_index] + [element_index] % 2 == 0 
+    if src[row_index][element_index] % 2 == 0 
       added << src[row_index] + [element_index]
     end
     element_index += 1
